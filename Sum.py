5bcd363ac7d5893bf6b53f73c6151882dd764178
palashMandals <pmandal@palash.tk>
2025-07-04 01:07:22 +0600

    sum.py

--- Sum.py
+++ Sum.py
@@ -1,4 +1,34 @@
 a = 10
 b = 20
 sum = a+b
-print("The sum is = ",sum)
+print("The sum is = ",sum)              #sumasion 
+
+print("\n")                              #new line 
+
+sub = a-b
+print("The sub is = ",sub)             #substracsion
+
+print("\n")                              #new line 
+
+mul = a*b
+print("The mul is = ",mul)             #multipecation
+
+print("\n")                              #new line 
+
+dev = a/b
+print("The dev is = ",dev)
+
+print("\n")                              #new line 
+
+mod = a%b
+print("The mod is = ",mod)
+
+print("\n")                              #new line 
+
+squar = a**b
+print("The square is = ",squar)
+
+print("\n")                              #new line 
+
+root= a//b
+print("the root is = ",root)
